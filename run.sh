69bc1cd13f49a8fad5dde4c29b3e74548f74e815
skramer1323 <37364121+skramer1323@users.noreply.github.com>
2018-11-20 13:34:20 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,8 @@ SONAR_HOST="${HOST:-http://localhost:9000}"
 SONAR_LOGIN_KEY="${LOGIN_KEY:-admin}"
 
 mono /opt/sonar-scanner-msbuild/SonarScanner.MSBuild.exe begin /d:sonar.host.url=$SONAR_HOST /d:sonar.login=$SONAR_LOGIN_KEY /k:$PROJECT_KEY /n:"$PROJECT_NAME" /v:$PROJECT_VERSION
-dotnet restore
-dotnet build
-dotnet test
+    - dotnet build $SOLUTION_FILE  --configuration Release
+    - echo !!!done build
+    - echo start tests
+    - find . -name "*.Test.csproj" -type f | xargs -n 1 -I '{}' bash -c 'echo !!test {} && dotnet test {}'
 mono /opt/sonar-scanner-msbuild/SonarScanner.MSBuild.exe end /d:sonar.login=$SONAR_LOGIN_KEY
